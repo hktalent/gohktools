ee956444b87deafd6df33cd81cdbf6272650e5df
51pwn MTX <hktalent@qq.com>
2022-03-01 18:08:23 +0800

    add lib

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ go 1.17
 module 51pwn.com/hktools
 
 require (
+	github.com/hashicorp/mdns v1.0.5 // indirect
+	github.com/jackpal/gateway v1.0.7 // indirect
+	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
 	github.com/olivere/elastic/v7 v7.0.31 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
---
+++ lib/mdns.go
@@ -0,0 +1,33 @@
+package lib
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/mdns"
+)
+
+func server4mdns(name, ipInfo string) {
+	// Setup our service export
+	host, _ := os.Hostname()
+	info := []string{ipInfo}
+	service, _ := mdns.NewMDNSService(host, name, "", "", 8000, nil, info)
+
+	// Create the mDNS server, defer shutdown
+	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	defer server.Shutdown()
+}
+
+func client4mdns(name string) {
+	// Make a channel for results and start listening
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	go func() {
+		for entry := range entriesCh {
+			fmt.Printf("Got new entry: %v\n", entry)
+		}
+	}()
+
+	// Start the lookup
+	mdns.Lookup(name, entriesCh)
+	close(entriesCh)
+}
