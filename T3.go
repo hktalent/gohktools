3aacad84533249f9c47a458b06ff413518e1cefb
51pwn MTX <hktalent@qq.com>
2022-03-01 13:53:54 +0800

    update

--- T3.go
+++ T3.go
@@ -2,34 +2,53 @@ package main
 
 import (
 	"bufio"
-	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net"
-	"net/http"
 	"net/url"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+
+	// "github.com/elastic/go-elasticsearch/v7"
+	elastic "github.com/olivere/elastic/v7"
 )
 
 /*
+// elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
 ./T3 -urlListsFile /Users/${USER}/MyWork/vulScanPro/T3.txt
+
+PUT _scripts/upPswd HTTP/1.1
+host:127.0.0.1:9200
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15
+Connection: close
+Content-Type: application/json;charset=UTF-8
+Content-Length: 256
+
+{
+    "script": {
+        "lang": "painless",
+        "source": "if(ctx._source.containsKey('weblogic')){if(params.containsKey('T3')){ctx._source.weblogic['T3']=params['T3']}if(params.containsKey('GIOP')){ctx._source.weblogic['GIOP']=params['GIOP']}}"
+    }
+}
 
 */
 var (
-	urlListsFile, esUrl, numThread string
-	saveIps                        bool
-	ins                            []chan int
+	urlListsFile, esUrl, numThread, tagName string
+	saveIps                                 bool
+	ctx11                                   context.Context
+	fd                                      *os.File
+	ins                                     []chan int
 )
 
 /*
 域名转换为ip
 */
-func addIps(url string) {
+func addIps(url string, in chan int) {
 	ip := url
 	if -1 < strings.Index(url, "://") {
 		a := regexp.MustCompile(`://`)
@@ -39,51 +58,125 @@ func addIps(url string) {
 		a := regexp.MustCompile(`/`)
 		ip = a.Split(ip, -1)[0]
 	}
-	if -1 < strings.Index(ip, ":") {
-		a := regexp.MustCompile(`:`)
-		ip = a.Split(ip, -1)[0]
+	if -1 == strings.Index(ip, ":") {
+		ip += ":80"
 	}
-
-	// fmt.Println(ip)
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", ip) //创建一个TCPAddr
-	if err != nil {
+	domain := ip
+	matched, _ := regexp.Match(`(\d{1,3}\.){3}\d{1,3}`, []byte(domain))
+	if matched {
+		in <- 1
 		return
 	}
+	// fmt.Println("[", ip, "]")
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ip) //创建一个TCPAddr
+	if err != nil {
+		fmt.Println(domain, err)
+		in <- 1
+		return
+	}
+
 	ip = fmt.Sprintf("%s", tcpAddr.IP)
-	fd, _ := os.OpenFile("Ips.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	fmt.Println(ip)
-	fd_content := strings.Join([]string{ip, "\n"}, "")
+	fmt.Println(domain, " ", ip)
+	fd_content := strings.Join([]string{domain, " ", ip, "\n"}, "")
+
+	fd, _ = os.OpenFile("Ips.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	fd.Write([]byte(fd_content))
 	fd.Close()
+	in <- 1
+}
+
+func IndexPrice(tweet map[string]interface{}) {
+
+	client, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
+	if err != nil {
+		fmt.Println("%v", err)
+		return
+	}
+
+	bulkRequest := client.Bulk()
+	request := elastic.NewBulkUpdateRequest()
+	req := request.Index("51pwn_index").Type("_doc").Id(tweet["url"].(string)).Doc(tweet).DocAsUpsert(true)
+	bulkRequest = bulkRequest.Add(req)
+	bulkResponse, err := bulkRequest.Do(ctx11)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if bulkResponse != nil {
+		fmt.Println(tweet["url"].(string))
+		fmt.Println(bulkResponse)
+	}
+
 }
 
 // 解决相同多次请求的问题
 func sendReq(szRst, url1 string, in chan int, szType, ip string) {
+	type mi = map[string]interface{}
+	// data1 := fmt.Sprintf(`{"url":"%s","ip":"%s","tagName":"%s","weblogic":{"%s":"%s"}}`, url1, ip, tagName, szType, url.QueryEscape(szRst))
+	xxData := mi{"url": url1, "ip": ip, "tagName": tagName, "weblogic": mi{szType: url.QueryEscape(szRst)}}
+	IndexPrice(xxData)
 	// oSave := json.NewDecoder(`{"id":url,"url":url,"weblogic":{"T3":szRst}}`)
-	escapeUrl := url.QueryEscape(url1)
-	post_body := bytes.NewReader([]byte(fmt.Sprintf(`{"url":"%s","ip":"%s","weblogic":{"%s":"%s"}}`, url1, ip, szType, url.QueryEscape(szRst))))
-	fmt.Println(szType, " ", ip)
-	req, err := http.NewRequest("POST", esUrl+escapeUrl, post_body)
-	if err == nil {
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15")
-		req.Header.Set("Content-Type", "application/json;charset=UTF-8")
-		req.Header.Set("Connection", "close")
-		// var rsp http.Response
-		rsp, err1 := http.DefaultClient.Do(req)
-		if err1 != nil {
-			fmt.Println(err1)
-			return
-		}
-		d, err := ioutil.ReadAll(rsp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		if -1 < strings.Index(string(d), `"successful":1`) {
-			fmt.Println(fmt.Sprintf("%s : %s  save ok", szType, url1))
-		}
-		defer rsp.Body.Close()
-		defer req.Body.Close()
-	}
+
+	// escapeUrl := url.QueryEscape(url1)
+	// post_body := bytes.NewReader([]byte(fmt.Sprintf(`{"doc_as_upsert":"true","doc": {"url":"%s","ip":"%s","tagName":"%s","weblogic":{"%s":"%s"}}}`, url1, ip, tagName, szType, url.QueryEscape(szRst))))
+	// fmt.Println(szType, " ", ip)
+	// req, err := http.NewRequest("POST", esUrl+escapeUrl+"/_update", post_body)
+	// if err == nil {
+	// 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15")
+	// 	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	// 	req.Header.Set("Connection", "close")
+	// 	// var rsp http.Response
+	// 	rsp, err1 := http.DefaultClient.Do(req)
+	// 	if err1 != nil {
+	// 		fmt.Println(err1)
+	// 		return
+	// 	}
+	// 	d, err := ioutil.ReadAll(rsp.Body)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	if -1 < strings.Index(string(d), `"successful":1`) {
+	// 		fmt.Println(fmt.Sprintf("%s : %s  save ok", szType, url1))
+	// 	} else {
+	// 		fmt.Println(string(d))
+	// 	}
+	// 	defer rsp.Body.Close()
+	// 	defer req.Body.Close()
+	// }
+
+	//  "http://127.0.0.1:9201"
+	// addresses := []string{"http://127.0.0.1:9200"}
+	// config := elasticsearch.Config{
+	// 	Addresses: addresses,
+	// 	Username:  "",
+	// 	Password:  "",
+	// 	CloudID:   "",
+	// 	APIKey:    "",
+	// }
+	// es, _ := elasticsearch.NewClient(config)
+	// // log.Println(elasticsearch.Version)
+	// // log.Println(es.Info())
+	// var buf bytes.Buffer
+	// doc := map[string]interface{}{
+	// 	"doc": map[string]interface{}{
+	// 		"url":     url1,
+	// 		"ip":      ip,
+	// 		"tagName": tagName,
+	// 		"weblogic": map[string]interface{}{
+	// 			szType: url.QueryEscape(szRst),
+	// 		},
+	// 	},
+	// }
+	// if err := json.NewEncoder(&buf).Encode(doc); err != nil {
+	// 	return
+	// }
+	// res, err := es.Update("51pwn_index", url1, &buf, es.Update.WithDocumentType("doc"))
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// log.Panicln(res.Body)
+	// defer res.Body.Close()
 	// fmt.Println("sendReq end")
 	// go http.Post(resUrl, "application/json",, post_body)
 }
@@ -105,8 +198,6 @@ func senData(url string, in chan int, szData, szCheck, szType string) {
 		// fmt.Println(ip)
 		tcpAddr, err := net.ResolveTCPAddr("tcp4", ip) //创建一个TCPAddr
 		if err != nil {
-			// fmt.Println("ResolveTCPAddr:")
-			// fmt.Println(err)
 			return
 		}
 
@@ -154,11 +245,16 @@ func sendT3(url string, in chan int) {
 
 func main() {
 	flag.StringVar(&urlListsFile, "urlListsFile", "/Users/"+os.Getenv("USER")+"/MyWork/vulScanPro/T3.txt", "url lists text file")
+	flag.StringVar(&tagName, "tagName", "gov,yh", "set list tag name,eg: gov")
+
 	flag.BoolVar(&saveIps, "saveIps", false, "save Ips to Ips.txt")
 
 	flag.StringVar(&esUrl, "esUrl", "http://127.0.0.1:9200/51pwn_index/_doc/", "es server url")
 	flag.StringVar(&numThread, "numThread", "16", "threads number")
 	flag.Parse()
+	// Create a context object for the API calls
+	ctx11 = context.Background()
+	wg := &sync.WaitGroup{}
 	numT, _e := strconv.Atoi(numThread)
 	if _e != nil {
 		numT = 16
@@ -171,8 +267,8 @@ func main() {
 		return
 	}
 	defer file.Close()
+
 	buf := bufio.NewReader(file)
-	nCnt := 0
 	for {
 		line, err := buf.ReadString('\n')
 		if err != nil {
@@ -185,38 +281,21 @@ func main() {
 			}
 		}
 		line = strings.TrimSpace(line)
-		if 10 < len(line) {
-			if -1 == strings.Index(line, "://") {
-				line = "http://" + line
+		wg.Add(1)
+		go func(line string) {
+			if 7 < len(line) {
+				if -1 == strings.Index(line, "://") {
+					line = "http://" + line
+				}
+				if saveIps {
+					addIps(line, ins)
+				} else {
+					sendGIOP(line, ins)
+					sendT3(line, ins)
+				}
 			}
-			if saveIps {
-				go addIps(line)
-				nCnt++
-			}
-
-			go sendGIOP(line, ins)
-			go sendT3(line, ins)
-			nCnt += 2
-		}
+			wg.Done()
+		}(line)
 	}
-	// fmt.Println(len(ins))
-	// for _, ch1 := range ins {
-	// 	<-ch1
-	// 	// fmt.Println(n)
-	// 	close(ch1)
-	// }
-	fmt.Println("总：", nCnt)
-	// XXOver:
-	for {
-		// select {
-		_, ok := <-ins
-		nCnt--
-		// fmt.Println("cur：", nCnt, "  ", ok)
-		fmt.Print("+")
-		if !ok || 0 >= nCnt {
-			break
-		}
-		// }
-	}
-	// close(ins)
+	wg.Wait()
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+go 1.17
+
+module 51pwn.com/hktools
+
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/olivere/elastic/v7 v7.0.31 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
